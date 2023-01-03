7667786bb3c89504a350a6b1a2cbec70d2d0d14b
DeathWind-sama <yychen_114@stu.xidian.edu.cn>
2023-01-03 21:56:39 +0800

    learn1 Done

--- learnVue/vue-learn1/src/V1.vue
+++ learnVue/vue-learn1/src/V1.vue
@@ -1,14 +1,17 @@
 <script>
 // 声明式渲染
-//这里定义的property无论定义如何都可以被template访问到
+//这里暴露出的各种property都可以被template访问到
 export default {
   data() {
     return {
-      num: 114,
+      num: 111,
       uname:"DeathWind",
       msg: "Hello, vue & vite!",
 
-      dynid:"vid"
+      dynid:"vid",
+      dyat:"id",
+
+      seen: false,
     };
   },
 };
@@ -17,11 +20,31 @@ export default {
 <template>
   <div>
     <!-- 双大括号使得值与组件中的对应项绑定 -->
-    <p>{{num}}</p>
     <p>{{ uname }}</p>
     <p>{{ msg }}</p>
+    <!-- 也可以塞json表达式。可见双括号只是标记此处要匹配变量名并赋值
+        也可以说，双括号本身就在解析一个js表达式
+    -->
+    <p>{{num+3}}</p>
+
     <!-- v-bind语法，可以绑定元素属性 -->
+    <!-- 如果objectOfAttrs是个内涵多个属性的对象，
+      可以用<div v-bind="objectOfAttrs"></div>同时赋多个值
+     -->
+    <!--完整写法： <p v-bind:id="dynid">My id is dynamic</p> -->
     <p :id="dynid">My id is dynamic</p>
+
+    <!-- 属性名也能动态化 -->
+    <p :[dyat]="dynid">My attribute is dynamic</p>
+
+    <!-- seen真假决定元素是否被移除 -->
+    <p v-if="seen">My Existence is Connected with seen</p>
+
+    <!-- JS表达式（必须可求值，可以有函数）可以被使用在如下场景上：
+      在文本插值中 (双大括号)
+      在任何 Vue指令(以v-开头的特殊属性)的值中 -->
+
+    <!-- 动态参数设为null时，相当于显式移除绑定 -->
   </div>
 </template>
 
--- learnVue/vue-learn1/src/main1.js
+++ learnVue/vue-learn1/src/main1.js
@@ -5,7 +5,7 @@ import V1 from './V1.vue'
 //这样id=app的东西就会加载V1.vue
 //'#app'也可以换成具体的dom元素
 //应用实例必须在调用了 .mount() 方法后才会渲染出来
-createApp(V1).mount('#app')
+createApp(V1).mount('#app') //绑定的元素找不到会导致undefined
 
 
 //直接写根组件模板，在app被mount之后就能读取其中的变量值
@@ -14,5 +14,5 @@ createApp(V1).mount('#app')
 //         return { count: 4 }
 //     }
 // })
-// const vm1=app1.mount('#app');
+// const vm1=app1.mount('#app');    
 // console.log(vm1.count);
