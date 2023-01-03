d7b7e14e14f5233a270d7bad6711f56a107e1e6d
DeathWind-sama <yychen_114@stu.xidian.edu.cn>
2023-01-03 17:15:21 +0800

    some vue

---
+++ learnVue/vue-learn1/src/V1.vue
@@ -0,0 +1,19 @@
+<script>
+// 声明式渲染
+//这里定义的property无论定义如何都可以被template访问到
+export default {
+  data() {
+    return {
+      num: 114,
+      uname:"DeathWind"
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p>{{num}}</p>
+    <p>{{ uname }}</p>
+  </div>
+</template>
---
+++ learnVue/vue-learn1/src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import V1 from './V1.vue'
+
+// import './assets/main.css'
+
+// createApp(App).mount('#app')
+createApp(V1).mount('#app')
+
+// const app1=Vue.createApp({
+//     data(){
+//         return { count: 4 }
+//     }
+// })
+
+// const vm1=app1.mount('#app');
+// console.log(vm1.count);
