3d75923b2da7a7bbdb7ba020df180f21d5448677
DeathWind-sama <yychen_114@stu.xidian.edu.cn>
2023-01-03 20:12:19 +0800

    clear default folds

--- learnVue/vue-learn1/src/V1.vue
+++ learnVue/vue-learn1/src/V1.vue
@@ -5,7 +5,10 @@ export default {
   data() {
     return {
       num: 114,
-      uname:"DeathWind"
+      uname:"DeathWind",
+      msg: "Hello, vue & vite!",
+
+      dynid:"vid"
     };
   },
 };
@@ -13,7 +16,18 @@ export default {
 
 <template>
   <div>
+    <!-- 双大括号使得值与组件中的对应项绑定 -->
     <p>{{num}}</p>
     <p>{{ uname }}</p>
+    <p>{{ msg }}</p>
+    <!-- v-bind语法，可以绑定元素属性 -->
+    <p :id="dynid">My id is dynamic</p>
   </div>
 </template>
+
+<style>
+#vid{
+  background-color: pink;
+}
+
+</style>
--- learnVue/vue-learn1/src/main.js
+++ learnVue/vue-learn1/src/main.js
@@ -1,17 +1,18 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import V1 from './V1.vue'
 
-// import './assets/main.css'
-
-// createApp(App).mount('#app')
+//根组件V1是被引入的V1.vue，将其绑定到id=app上
+//这样id=app的东西就会加载V1.vue
+//'#app'也可以换成具体的dom元素
+//应用实例必须在调用了 .mount() 方法后才会渲染出来
 createApp(V1).mount('#app')
 
-// const app1=Vue.createApp({
+
+//直接写根组件模板，在app被mount之后就能读取其中的变量值
+// const app1=createApp({
 //     data(){
 //         return { count: 4 }
 //     }
 // })
-
 // const vm1=app1.mount('#app');
 // console.log(vm1.count);
